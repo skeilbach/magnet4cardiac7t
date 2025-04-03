c34d4ad5f4496a62aff81129d574f1e018302556
Andrzej Dulny <andzej.dulny@gmail.com>
2025-04-03 13:19:46 +0200

    Abstract SAR and B1 calculation to utils

--- src/costs/b1_homogeneity.py
+++ src/costs/b1_homogeneity.py
@@ -1,5 +1,6 @@
 from .base import BaseCost
 from ..data.simulation import SimulationData
+from ..data.utils import B1Calculator
 
 import numpy as np
 
@@ -7,18 +8,11 @@ import numpy as np
 class B1HomogeneityCost(BaseCost):
     def __init__(self) -> None:
         super().__init__()
-        self.direction = "minimize"
-        
-    def _calculate_b1_field(self, field: np.ndarray) -> np.ndarray:
-        b_field = field[1]
-
-        # b1_plus = b_x + i*b_y
-        b_field_complex = b_field[0] + 1j*b_field[1]
-        b1_plus = 0.5*(b_field_complex[0] + 1j*b_field_complex[1])
-        return b1_plus
+        self.direction = "maximize"
+        self.b1_calculator = B1Calculator()
 
     def calculate_cost(self, simulation_data: SimulationData) -> float:
-        b1_field = self._calculate_b1_field(simulation_data.field)
+        b1_field = self.b1_calculator(simulation_data)
         subject = simulation_data.subject
         
         b1_field_abs = np.abs(b1_field)
